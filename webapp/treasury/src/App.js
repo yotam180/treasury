11a37fb9b1dc406ba36049943606ef76d870e8ea
Yotam Salmon <yotam.salmon@gmail.com>
2020-10-09 22:47:11 +0300

    Add info to repository endpoint and create repo view for webapp

--- webapp/treasury/src/App.js
+++ webapp/treasury/src/App.js
@@ -9,6 +9,7 @@ import { theme } from "./style/theme";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
 import { ReposView } from "./views/ReposView";
+import { RepoView } from "./components/RepoView";
 
 const useStyles = makeStyles(() => ({
   fixedWrapper: {
@@ -41,7 +42,26 @@ function App() {
                 <ReposView />
               </Route>
               <Route path="/repo/:name">
-                <div>This is a repo view</div>
+                <RepoView
+                  data={[
+                    {
+                      version: "0.1.0-alpha",
+                      last_updated: "2020-10-09T14:20:47.234204+03:00",
+                    },
+                    {
+                      version: "0.1.1",
+                      last_updated: "2020-10-08T23:40:47.2057345+03:00",
+                    },
+                    {
+                      version: "0.2.3",
+                      last_updated: "2020-10-08T23:40:52.7862415+03:00",
+                    },
+                    {
+                      version: "0.3.0",
+                      last_updated: "2020-10-09T11:15:38.5226762+03:00",
+                    },
+                  ]}
+                />
               </Route>
             </Switch>
           </div>
