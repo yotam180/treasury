fe7af0c190b89094b365f913407e638284ade8f0
Yotam Salmon <yotam.salmon@gmail.com>
2020-10-09 19:11:42 +0300

    Add more UI stuff to the webapp skeleton

--- webapp/treasury/src/App.js
+++ webapp/treasury/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./style/App.css";
+import "./style/Fonts.css";
+import "@material-ui/core/styles/";
+
+import {
+  AppBar,
+  Button,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <ThemeProvider>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar edge="start">hello world</Toolbar>
+        </AppBar>
+        <Button variant="contained">Hello world</Button>
+        hello world
+      </ThemeProvider>
     </div>
   );
 }
